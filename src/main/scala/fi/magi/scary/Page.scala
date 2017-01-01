82b04c38b61b27f1911c8a351085e7e709db284b
Marko Gronroos <magi@vaadin.com>
2017-01-01 21:37:51 +0200

    Minor additions towards minimum viable product. Added a Page class.

---
+++ src/main/scala/fi/magi/scary/Page.scala
@@ -0,0 +1,29 @@
+package fi.magi.scary
+
+import javax.servlet.http.HttpServletResponse
+
+/**
+ * Page object reads and generates the page.
+ */
+class Page(val path: String) {
+  def generate(response: HttpServletResponse) = {
+    val original = read(path)
+    var outs = response.getOutputStream
+    outs.println("path=" + path)
+    
+    // Null path should be treated same as root path
+    // TODO Path with or without terminating slash may refer to a directory
+    val cleanpath =
+      if (path == null) "/" else path
+
+    // val template = templates.match(url)
+  }
+  
+  def checkexists(filename: String) : Boolean = {
+    false
+  }
+
+  def read(path: String): String = {
+    null
+  }
+}
--- src/main/scala/fi/magi/scary/ScaryServlet.scala
+++ src/main/scala/fi/magi/scary/ScaryServlet.scala
@@ -7,13 +7,28 @@ import javax.servlet.annotation.WebServlet
 
 /**
  * Dynamic page generator.
- * 
+ *
  * Stub, does not do anything yet.
  */
-@WebServlet(name="ScaryServlet", urlPatterns=Array("/*"))
+@WebServlet(name = "ScaryServlet", urlPatterns = Array("/*"))
 class ScaryServlet extends HttpServlet {
-  override def service(request:HttpServletRequest, response:HttpServletResponse) = { 
+  private final val scary_webroot = "scary.webroot"
+  
+  override def service(request: HttpServletRequest, response: HttpServletResponse) = {
     var outs = response.getOutputStream
     outs.println("Hello, world!")
+    
+    // Get webroot path
+    val env = System.getenv
+    outs.println(
+      if (env.containsKey(scary_webroot)) {
+        val webroot: String = env.get(scary_webroot)
+        "webroot=" + webroot
+      } else
+        "No webroot")
+
+    val path = request.getPathInfo
+    val page = new Page(path)
+    page.generate(response)
   }
 }
